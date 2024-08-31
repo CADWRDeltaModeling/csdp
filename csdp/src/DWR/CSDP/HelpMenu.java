4ac7f474eeb5b0ac5fd5d8e641a98a743fffe7dd
Tom <Bradley.Tom@water.ca.gov>
2024-08-30 17:03:21 -0700

    removed license information

--- csdp/src/DWR/CSDP/HelpMenu.java
+++ csdp/src/DWR/CSDP/HelpMenu.java
@@ -1,43 +1,3 @@
-/*
-    Copyright (C) 1998 State of California, Department of Water
-    Resources.
-
-    This program is licensed to you under the terms of the GNU General
-    Public License, version 2, as published by the Free Software
-    Foundation.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, contact Dr. Francis Chung, below,
-    or the Free Software Foundation, 675 Mass Ave, Cambridge, MA
-    02139, USA.
-
-    THIS SOFTWARE AND DOCUMENTATION ARE PROVIDED BY THE CALIFORNIA
-    DEPARTMENT OF WATER RESOURCES AND CONTRIBUTORS "AS IS" AND ANY
-    EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-    IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-    PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE CALIFORNIA
-    DEPARTMENT OF WATER RESOURCES OR ITS CONTRIBUTORS BE LIABLE FOR
-    ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-    CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
-    OR SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA OR PROFITS; OR
-    BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-    LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
-    USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
-    DAMAGE.
-
-    For more information, contact:
-
-    Dr. Francis Chung
-    California Dept. of Water Resources
-    Division of Planning, Delta Modeling Section
-    1416 Ninth Street
-    Sacramento, CA  95814
-    916-653-5601
-    Chung@water.ca.gov
-
-    or see our home page: http://wwwdelmod.water.ca.gov/
-*/
 package DWR.CSDP;
 
 import java.awt.event.ActionEvent;
@@ -68,14 +28,6 @@ public class HelpMenu {
 			_gui = gui;
 			_licenseMessage = "Cross-section Development Program(CSDP) Version " + CsdpFunctions.getVersion() + "\n";
 			i++;
-			_licenseMessage += "by Brad Tom" + "\n";
-			i++;
-			_licenseMessage += "State of California, Department of Water Resources." + "\n";
-			i++;
-			_licenseMessage += "Send bug reports to Bradley.Tom@water.ca.gov or call (916) 654-8540." + "\n";
-			i++;
-			_licenseMessage += "" + "\n";
-			i++;
 			_licenseMessage += "Copyright (C) 1998 State of California, Department of Water" + "\n";
 			i++;
 			_licenseMessage += "Resources." + "\n";
@@ -90,7 +42,7 @@ public class HelpMenu {
 			i++;
 			_licenseMessage += "You should have received a copy of the GNU General Public License" + "\n";
 			i++;
-			_licenseMessage += "along with this program; if not, contact Dr. Francis Chung, below," + "\n";
+			_licenseMessage += "along with this program; if not, contact Brad Tom, below," + "\n";
 			i++;
 			_licenseMessage += "or the Free Software Foundation, 675 Mass Ave, Cambridge, MA" + "\n";
 			i++;
@@ -132,19 +84,17 @@ public class HelpMenu {
 			i++;
 			_licenseMessage += "" + "\n";
 			i++;
-			_licenseMessage += "Dr. Francis Chung" + "\n";
+			_licenseMessage += "Brad Tom" + "\n";
 			i++;
 			_licenseMessage += "California Dept. of Water Resources" + "\n";
 			i++;
-			_licenseMessage += "Office of State Water Project Planning, Delta Modeling Section" + "\n";
+			_licenseMessage += "Modeling Support Office, Delta Modeling Section" + "\n";
 			i++;
-			_licenseMessage += "1416 Ninth Street" + "\n";
+			_licenseMessage += "1516 Ninth Street" + "\n";
 			i++;
 			_licenseMessage += "Sacramento, CA  95814" + "\n";
 			i++;
-			_licenseMessage += "916-653-5601" + "\n";
-			i++;
-			_licenseMessage += "chung@water.ca.gov" + "\n";
+			_licenseMessage += "Bradley.Tom@water.ca.gov" + "\n";
 			i++;
 			_licenseMessage += "" + "\n";
 			i++;
--- csdp/src/DWR/CSDP/UndoableJTextArea.java
+++ csdp/src/DWR/CSDP/UndoableJTextArea.java
@@ -1,6 +1,5 @@
 package DWR.CSDP;
 
-
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Toolkit;
